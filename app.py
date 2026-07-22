9a3d607752530e5d5fbf5d1c73dcb29d263559c2
st25095 <st25095@ormiston.school.nz>
2026-07-22 14:21:38 +1200

    Setup basic folder structure and created HTML templates, CSS, Python file, and JSON with pizzas.

--- app.py
+++ app.py
@@ -8,9 +8,19 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 
+def load_data():
+    try:
+        with open('data/pizzas.json') as file:
+            pizzas = json.load(file)
+        return pizzas
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error loading data: {e}")
+        return {}
+
 @app.route('/')
 def home():
-    return render_template("home.html")
+    pizzas = load_data()
+    return render_template("home.html", pizzas = pizzas)
 
 @app.route('/about')
 def about():
